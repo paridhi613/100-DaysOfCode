0b78e86bd51b4cb1a4b8db3073601e9b78dac22f
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-22 20:38:35 +0530

    added

--- 5_typecasting.py
+++ 5_typecasting.py
@@ -3,4 +3,11 @@ b="4"
 print(a+b)  #this will give 34 becoz a and b are string 
 
 print(int(a)+int(b))   #this will give 7 as string are converted into integer using typecasting
+#this is explicit typecasting as we are doing it manually
+
+
+d= 5.5
+f=4
+print(d+f)  #this will give 9.5 as d is float and f is int but python will convert f into float and then do the addition
+#this is implicit typecasting as python is doing it automatically
 
