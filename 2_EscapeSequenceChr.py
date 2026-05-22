15a6d772c01acc74fecb2b021aff18ef7e9a0127
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-22 13:54:12 +0530

    added

--- 2_EscapeSequenceChr.py
+++ 2_EscapeSequenceChr.py
@@ -2,3 +2,7 @@ print("Hello! We are \"BTS\". \nWe are \"seven\" normal boys from Korea.")
 
 # \n is used to create a new line
 # \" is used to print double quotes inside a string
+
+
+print("non","empty", sep="-", end="\n")
+print("hello")
