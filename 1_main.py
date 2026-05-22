579463eb36f95f8e54e31a1d0dcd66a9dae23749
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-22 13:25:17 +0530

    added

--- 1_main.py
+++ 1_main.py
@@ -13,3 +13,17 @@ alt + down arrow to move the above line down
 alt + up arrow to move the above line up
 
 ''' 
+
+
+
+"""
+
+this is also multi line comment
+
+alt + down arrow to move the above line down
+alt + up arrow to move the above line up
+
+
+shift + home or end to select from the current position to the beginning or end of the line
+
+""" 
