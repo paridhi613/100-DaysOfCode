e98804d0e1abc15606a59d7d24e902c744754f5e
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-24 16:02:20 +0530

    added

---
+++ 7_slicing.py
@@ -0,0 +1,10 @@
+fruit = "mango"
+len =  len(fruit)    #len finction is used to find the length of the string
+print("length of mango is ", len)
+
+#slicing 
+print(fruit[0:3])   #man 
+print(fruit[1:4])   #ang
+print(fruit[2:])    #ngo
+print(fruit[:4])   #mang
+print(fruit[:])    #mango
