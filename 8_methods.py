7d7ce2c2ce9d6e41c8b3d913d883f495d1922677
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-27 16:28:41 +0530

    added

--- 8_methods.py
+++ 8_methods.py
@@ -14,6 +14,7 @@ intro = "my name is jimin"
 print(intro.capitalize())   #My name is jimin  the first character of the string will be capitalized
 print(len(intro))   #16  the length of the string will be printed
 
+
 intro1 = "my name is jimin"
 print(intro1.center(50))   #                 my name is jimin                  the string will be centered within a field of 50 characters
 print(len(intro1.center(50)))   #50  the length of the string along with the spaces will be printed
