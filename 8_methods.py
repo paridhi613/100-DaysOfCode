766fd6ae5efbd154e75d0822f50bc28c7c488ecd
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-27 16:27:02 +0530

    added

--- 8_methods.py
+++ 8_methods.py
@@ -12,3 +12,8 @@ print(song.split())   #['silver', 'spoon']  the string will be split into a list
 
 intro = "my name is jimin"
 print(intro.capitalize())   #My name is jimin  the first character of the string will be capitalized
+print(len(intro))   #16  the length of the string will be printed
+
+intro1 = "my name is jimin"
+print(intro1.center(50))   #                 my name is jimin                  the string will be centered within a field of 50 characters
+print(len(intro1.center(50)))   #50  the length of the string along with the spaces will be printed
