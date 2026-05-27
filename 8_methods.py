71521929e8368c9ed1449989b31de578452d73d6
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-27 15:55:11 +0530

    added

--- 8_methods.py
+++ 8_methods.py
@@ -4,3 +4,7 @@ print(name.rstrip("!"))   #jiminnn   #given jimin wihtout the ! at the end
 
 name="harry"
 print(name.replace("harry","josh"))   #josh  harry will be replaced by josh
+
+
+song="silver spoon"
+print(song.split())   #['silver', 'spoon']  the string will be split into a list of words
