a8ce97b7f820ef5203ea55bad89f3173daea1291
paridhi613 <kalyaneparidhi94@gmail.com>
2026-05-27 15:52:08 +0530

    added

--- 8_methods.py
+++ 8_methods.py
@@ -1,2 +1,6 @@
 name="jiminnn!!!!!"
 print(name.rstrip("!"))   #jiminnn   #given jimin wihtout the ! at the end 
+
+
+name="harry"
+print(name.replace("harry","josh"))   #josh  harry will be replaced by josh
