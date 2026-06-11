8f5fa3d52244eae97495fcd3d356745d2aace2ae
paridhi613 <kalyaneparidhi94@gmail.com>
2026-06-11 23:41:37 +0530

    added

--- 8_methods2.py
+++ 8_methods2.py
@@ -1,3 +1,5 @@
 str="hellomynameisjungkook"
 print(str.isalnum())      #return true because strings are only present in continuous manner without any space or special characters
 
+str1="hello my name is jungkook"
+print(str1.isalnum())     #return false because there are spaces in the string
