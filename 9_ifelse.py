f4b89c7370d380e05523c347e66bb2b49fd39739
paridhi613 <kalyaneparidhi94@gmail.com>
2026-06-12 16:44:58 +0530

    added

--- 9_ifelse.py
+++ 9_ifelse.py
@@ -18,3 +18,17 @@ if (apple <= budget):
     print("add 1kg apple in cart")
 else:
     print("you cant buy apple")
+
+
+
+print()
+
+
+
+num=int(input("enter a number: "))
+if num < 0:
+    print("number is negative")
+elif num == 0:
+    print("number is zero")
+else:
+    print("number is positive") 
