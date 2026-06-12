00d8eafd4eb8c4f43f1c1e43677ae20b1ee0f145
paridhi613 <kalyaneparidhi94@gmail.com>
2026-06-12 13:38:15 +0530

    added

--- 9_ifelse.py
+++ 9_ifelse.py
@@ -5,3 +5,16 @@ if a>=18:
     print("you can drive")
 else:
     print("you cant drive")
+
+
+
+print()
+
+
+apple=200
+budget=240
+
+if (apple <= budget):
+    print("add 1kg apple in cart")
+else:
+    print("you cant buy apple")
